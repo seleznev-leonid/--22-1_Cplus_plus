174e9db664b666cefc49730f915a867ff62e385e
seleznev-leonid <86149262+seleznev-leonid@users.noreply.github.com>
2024-09-21 09:59:31 +0300

    Update Для_произвольных.cpp

--- Для_произвольных.cpp
+++ Для_произвольных.cpp
@@ -1,22 +1,6 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-
 #include <iostream>
 #include <cmath>
 using namespace std;
-
-
-
-
-
-
-
 
 // 1.Для произвольных ...
 
